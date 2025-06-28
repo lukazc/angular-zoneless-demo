b475e6f3263005d31267ea4debf0c21edeb53702
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2025-06-28 09:56:35 +0200

    Define application routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard/dashboard';
+import { PokemonList } from './features/pokemon/pokemon-list/pokemon-list';
+import { PokemonDetail } from './features/pokemon/pokemon-detail/pokemon-detail';
+import { NotFound } from './shared/components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: Dashboard },
+    { path: 'pokemon', children: [
+        { path: '', component: PokemonList },
+        { path: 'detail/:id', component: PokemonDetail }
+    ]},
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: '**', component: NotFound }
+];
