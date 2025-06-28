532cf877821c6381953ba91f2abfac78ffa171bd
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2025-06-28 09:26:53 +0200

    Add HttpClient provider to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -7,6 +8,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+        withFetch(),
+        // withInterceptors([loadingInterceptor, errorInterceptor])
+    )
   ]
 };
