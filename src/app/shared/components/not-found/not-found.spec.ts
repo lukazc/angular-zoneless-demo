cc701ad3616c2411e7878bccb48d3e8b766a9fa3
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2025-06-28 13:22:27 +0200

    Implement NotFound component.

--- src/app/shared/components/not-found/not-found.spec.ts
+++ src/app/shared/components/not-found/not-found.spec.ts
@@ -1,27 +1,38 @@
+import { Component, provideZonelessChangeDetection } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { NotFound } from './not-found';
-import { provideZonelessChangeDetection } from '@angular/core';
+
+@Component({ template: '' })
+class MockDashboard { }
 
 describe('NotFound', () => {
-    let component: NotFound;
     let fixture: ComponentFixture<NotFound>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [NotFound],
             providers: [
-                provideZonelessChangeDetection()
+                provideZonelessChangeDetection(),
             ]
-        })
-            .compileComponents();
+        }).compileComponents();
 
         fixture = TestBed.createComponent(NotFound);
-        component = fixture.componentInstance;
         await fixture.whenStable();
     });
 
     it('should create', () => {
-        expect(component).toBeTruthy();
+        expect(fixture.componentInstance).toBeTruthy();
+    });
+
+    it('should display "Page Not Found" title', () => {
+        const title = fixture.debugElement.query(By.css('.error-title'));
+        expect(title).toBeTruthy();
+    });
+
+    it('should have a home button', () => {
+        const button = fixture.debugElement.query(By.css('.home-button'));
+        expect(button).toBeTruthy();
     });
 });
--- src/app/shared/components/not-found/not-found.ts
+++ src/app/shared/components/not-found/not-found.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-not-found',
-  imports: [],
-  templateUrl: './not-found.html',
-  styleUrl: './not-found.scss'
+    selector: 'app-not-found',
+    imports: [
+        RouterLink,
+        MatButtonModule,
+        MatIconModule
+    ],
+    templateUrl: './not-found.html',
+    styleUrl: './not-found.scss'
 })
-export class NotFound {
-
-}
+export class NotFound {}
