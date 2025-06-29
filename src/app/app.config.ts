17ce247d7f47f7e78078b8039c3ac7e220a63d6e
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2025-06-29 20:35:37 +0200

    Add @angular/animations dependency and provideAnimations in app configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+    provideAnimations(),
     provideRouter(routes),
     provideHttpClient(
         withFetch(),
